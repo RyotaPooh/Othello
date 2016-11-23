d41e1a44f8587086761713647c7c077b214981f4
xpheal <xpheal@gmail.com>
2016-11-23 01:21:00 -0600

    greedy random

--- ai.py
+++ ai.py
@@ -23,7 +23,7 @@ class AI_Random(AI):
 
 		return possible_moves[randint(0, len(possible_moves) - 1)]
 
-class AI_Greedy(AI):
+class AI_Greedy_First(AI):
 	def __init__(self, name, piece):
 		AI.__init__(self, name, piece)
 
@@ -43,3 +43,26 @@ class AI_Greedy(AI):
 							max_flanked = len(flanked_piece)
 
 		return best_move
+
+class AI_Greedy_Random(AI):
+	def __init__(self, name, piece):
+		AI.__init__(self, name, piece)
+
+	def get_move(self, board):
+		# Array of Tuple (row, col) of all possible moves
+		best_moves = []
+		max_flanked = 0
+
+		for row in range(board.size):
+			for col in range(board.size):
+				if board.move_valid(row, col):
+					flanked_piece = board.get_flanked(row, col)
+
+					if flanked_piece:
+						if max_flanked < len(flanked_piece):
+							best_moves = [(row, col)]
+							max_flanked = len(flanked_piece)
+						elif max_flanked == len(flanked_piece):
+							best_moves += [(row, col)]
+
+		return best_moves[randint(0, len(best_moves) - 1)]
